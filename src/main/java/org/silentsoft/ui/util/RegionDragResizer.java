f2d218083c0c6729d9954fcad0bee708ad2e5cbd
silentsoft <hyesung.lee@silentsoft.org>
2016-04-06 00:52:13 +0900

    Applied drag direction to all(NW, NE, SE, SW, E, W, S, N)

--- src/main/java/org/silentsoft/ui/util/RegionDragResizer.java
+++ src/main/java/org/silentsoft/ui/util/RegionDragResizer.java
@@ -2,6 +2,9 @@ package org.silentsoft.ui.util;
 
 import javafx.scene.layout.Region;
 
+/**
+ * @author silentsoft
+ */
 public class RegionDragResizer extends DragResizer {
 
 	private RegionDragResizer(Region region) {
